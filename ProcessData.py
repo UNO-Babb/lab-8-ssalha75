9908cb088043d3cc12dcdf7e28827ce5240598b4
ssalha75 <ssalha@unomaha.edu>
2025-04-02 14:57:11 +0000

    Done with Lab 8.

--- ProcessData.py
+++ ProcessData.py
@@ -1,9 +1,28 @@
 #ProcessData.py
-#Name:
-#Date:
-#Assignment:
+#Name: Sara Salha
+#Date: 4/2/25
+#Assignment: Lab 8
 
-import random
+def makeID(first, last, num):
+  while len(last) < 5:
+    last = last + "X"
+  last3 = num[-3:]
+  id = first[0] + last + last3
+
+  return id
+def makeMajorYear(major, year):
+  first3 = major[0:3]
+  year = year.upper()
+  if year == "FRESHMAN":
+    yearAb = "FR"
+  elif year == "SOPHOMORE":
+    yearAb = "SO"
+  elif year == "JUNIOR":
+    yearAb = "JU"
+  elif year == "SENIOR":
+    yearAb = "SE"
+  majorYear = first3 + "-" + yearAb
+  return majorYear
 
 def main():
 
@@ -12,8 +31,19 @@ def main():
   outFile = open("StudentList.csv", 'w')
 
   #Process each line of the input file and output to the CSV file
+  for student in inFile:
+    #student = "Antwan Dougherty AntwanDougherty@yahoo.com 443-13-3556 03/28/1996 Freshman Philosophy"
+    studentData = student.split()
+    firstName = studentData[0]
+    lastName = studentData[1]
+    studentID = studentData[3]
+    year = studentData[5]
+    major = studentData[6]
+    userID = makeID(firstName, lastName, studentID)
+    majorYear = makeMajorYear(major, year)
 
-
+    studentOutput = lastName + "," + firstName + "," + userID + "," + majorYear + "\n"
+    outFile.write(studentOutput)
 
   #Close files in the end to save and ensure they are not damaged.
   inFile.close()
